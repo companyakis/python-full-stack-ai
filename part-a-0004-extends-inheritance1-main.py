8984fdc1fc83e209c6c8908025ae7acac1962380
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-03-03 09:13:05 +0300

    Create part-a-0004-extends-inheritance1-main.py

---
+++ part-a-0004-extends-inheritance1-main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# extends and inheritance
+
+@app.route("/somedata")
+def somedata():
+  
+    return render_template("somedata.html")
+
+@app.route("/")
+def main():
+    
+    year = 2025
+    
+    who_i_am = "Mustafa Büyükdereli"
+    
+    some_data = {"USA": "$", "Europe": "€", "Turkiye": "₺"}
+    
+    return render_template("index.html", year = year, who_i_am = who_i_am, some_data = some_data)
+
+@app.route("/products")
+def products():
+  
+    return render_template("products.html")
+
+@app.route("/about")
+def about():
+  
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
